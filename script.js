b9a40e7a2244546c9b19c372b2469357d96dec3c
oxidetor <prateek.girdhar@outlook.com>
2022-06-11 17:13:22 -0700

    Add button to change grid resolution

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ body.appendChild(container);
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
-  square.innerText = i + 1;
+  square.style.height = "60px";
+  square.style.width = "60px";
   container.appendChild(square);
+}
+
+function changeResolution() {
+  let newSize = +prompt("Enter new grid size (max: 100)");
+  console.log(newSize);
+  container.textContent = "";
+  for (let i = 0; i < newSize * newSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    const squareSize = 960 / newSize;
+    square.style.height = `${squareSize}px`;
+    square.style.width = `${squareSize}px`;
+    container.appendChild(square);
+  }
 }
