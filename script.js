6a451ab059ce813b578c36e93e586abd7e6a8ff1
oxidetor <prateek.girdhar@outlook.com>
2022-06-11 18:01:23 -0700

    Add init function, global constants

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-const body = document.querySelector("body");
+const CONTAINER_WIDTH = 960;
+const INIT_SQUARE_WIDTH = 40;
+const INIT_SQUARE_HEIGHT = 40;
+const INIT_GRID_SIZE = 16;
 
-const container = document.createElement("div");
-container.classList.add("container");
+window.onload = init;
 
-body.appendChild(container);
-
-generateGrid(16);
-
-function changeResolution() {
-  let newSize = +prompt("Enter new grid size (max: 100)");
-  emptyGrid();
-  generateGrid(newSize);
-}
-
-function emptyGrid() {
-  container.textContent = "";
+function init() {
+  const container = document.querySelector(".container");
+  container.style.width = `${CONTAINER_WIDTH}px`;
+  generateGrid(INIT_GRID_SIZE);
 }
 
 function generateGrid(size) {
@@ -25,19 +19,31 @@ function generateGrid(size) {
 }
 
 function createSquare() {
+  const container = document.querySelector(".container");
   const square = document.createElement("div");
   square.classList.add("square");
-  square.addEventListener("mouseenter", onMouseEnter);
+  square.addEventListener("mouseenter", onMouseEnterSquare);
   container.appendChild(square);
   return square;
 }
 
+function onMouseEnterSquare(e) {
+  e.target.style.backgroundColor = "red";
+}
+
 function setSquareDimensions(square, size) {
-  const squareSize = 960 / size;
+  const squareSize = CONTAINER_WIDTH / size;
   square.style.height = `${squareSize}px`;
   square.style.width = `${squareSize}px`;
 }
 
-function onMouseEnter(e) {
-  e.target.style.backgroundColor = "red";
+function changeGridSize() {
+  let newSize = +prompt("Enter new grid size (max: 100)");
+  emptyGrid();
+  generateGrid(newSize);
+}
+
+function emptyGrid() {
+  const container = document.querySelector(".container");
+  container.textContent = "";
 }
